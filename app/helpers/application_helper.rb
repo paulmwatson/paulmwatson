08f5fd37664677e2505516d7bbf7bb70b7a8fda2
paulmwatson <github.com@paulmwatson.com>
2020-06-17 16:20:13 +0200

    Two new photos and tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def posts
+    %w[fish plumjam mercedes cx5 cabbage pringlesky houses firebridge artbridge echobridge glitchbridge drl reflection]
+  end
 end
--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class SiteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get site_index_url
+  test 'should get index' do
+    get root_url
     assert_response :success
   end
 
+  test 'should get post' do
+    get post_url(id: 'drl')
+    assert_response :success
+  end
 end
